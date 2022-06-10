318fd6a53ea45e1100f9d67268410714a030ab00
Larold <laroldsjubilantjunkyard@gmail.com>
2022-06-09 23:07:54 -0400

    updating comments

--- generate-graphics.bat
+++ generate-graphics.bat
@@ -9,11 +9,11 @@ mkdir "headers\gen\graphics"
 SET GBDK_HOME=C:/gbdk
 SET PNG2ASSET=%GBDK_HOME%/bin/png2asset.exe
 
-:: Tetrominos
+:: Sprites
 %PNG2ASSET% graphics\paddle.png -c source\gen\graphics\paddle.c  -sw 32 -sh 8 -spr8x8 -keep_palette_order -noflip
 %PNG2ASSET% graphics\ball.png -c source\gen\graphics\ball.c -sw 8 -sh 8 -spr8x8 -keep_palette_order -noflip
 
-:: Backgrounds & Maps
+:: Background Tiles
 %PNG2ASSET% graphics\UserInterface.png -c source\gen\graphics\UserInterface.c -map -use_map_attributes  -keep_palette_order -noflip
 %PNG2ASSET% graphics\Bricks.png -c source\gen\graphics\Bricks.c -map -use_map_attributes  -keep_palette_order -noflip
 %PNG2ASSET% graphics\Font.png -c source\gen\graphics\Font.c -map -use_map_attributes  -keep_palette_order -noflip
